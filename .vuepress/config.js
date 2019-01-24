05ff86f2eb4755742202fea2d94553e72e763e65
Marco Mark <m2de@outlook.com>
2019-01-24 09:01:18 +0000

    Include new category in menu

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -15,6 +15,7 @@ module.exports = {
             '/models/',
             '/tinker/',
             '/testing/',
+            '/dusk/',
         ],
         lastUpdated: 'Last Updated',
         serviceWorker: {
